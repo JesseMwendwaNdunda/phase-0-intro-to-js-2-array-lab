a28a42eac45e51e88a34e928c9801ec20f76cd1c
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-06-17 11:50:53 +0300

    add the changes made

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+// Initial array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive functions
+function appendCat(name) {
+  return [...cats, name]; // returns new array with name added at the end
+}
+
+function prependCat(name) {
+  return [name, ...cats]; // returns new array with name added at the beginning
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1); // returns a new array without the last cat
+}
+
+function removeFirstCat() {
+  return cats.slice(1); // returns a new array without the first cat
+}
+
